6f874bdef7a4e18462aeee65a1357d68cf4ea412
Stef_DP <stefano.delprete.08@gmail.com>
2024-01-13 13:32:35 +0100

    Create idk.js

---
+++ domains/idk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: '...'
+  domain: 'is-an.app'
+  subdomain: 'myapp'
+  owner: {
+    repo: '<https://URL/to/the/repository/with/your/project/sources/if/it/is/public>',
+    email: 'admin@stefdp.is-a.dev',
+  },
+  record: {
+    CNAME: 'stefdp.is-a.dev'
+  }
+})
